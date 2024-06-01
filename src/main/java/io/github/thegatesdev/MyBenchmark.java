ef1493591eab9fbc00beea3bbe8b0992c2381d78
Timar Karels <69715898+thegatesdev@users.noreply.github.com>
2024-06-01 12:06:12 +0200

    Initial JMH setup

---
+++ src/main/java/io/github/thegatesdev/MyBenchmark.java
@@ -0,0 +1,12 @@
+package io.github.thegatesdev;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+public class MyBenchmark {
+
+    @Benchmark
+    public void testMethod() {
+        
+    }
+
+}
